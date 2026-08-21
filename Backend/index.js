3c702bcecae0c35dc3e013e33ba54023769f50f9
Alok Kumar <alokkumar68338@gmail.com>
2026-08-21 21:01:59 +0530

    Change CORS origin for production environment
    Updated CORS origin to allow requests from the production URL.

--- Backend/index.js
+++ Backend/index.js
@@ -27,7 +27,7 @@ const io = new Server(server, {
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://axion-go.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   })
